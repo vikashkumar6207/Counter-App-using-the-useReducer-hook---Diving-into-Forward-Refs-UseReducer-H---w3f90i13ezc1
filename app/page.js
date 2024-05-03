3c9296f3ade8f4396b54167cae4f43e1075c93f8
vikashkumar6207 <136398175+vikashkumar6207@users.noreply.github.com>
2024-05-03 14:28:40 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div id="main">
+      <h2>Task Counter</h2>
+      {/*<button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
+      <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
+  <h3 className="counter">Number of Tasks : {taskState.count}</h3>*/}
+
+    {/*Update the above code to achieve the solution*/}
+    </div>
+  )
+}
