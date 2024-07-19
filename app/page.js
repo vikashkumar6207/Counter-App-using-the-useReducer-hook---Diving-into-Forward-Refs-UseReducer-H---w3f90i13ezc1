4259a4d24c72b5600415b2f1c6c0e7769f2eb836
vikashkumar6207 <vktd828403@gmail.com>
2024-07-19 12:07:03 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,15 +1,42 @@
-import React from "react"
+"use client"
+import React, { useReducer } from "react";
 
 export default function Home() {
+  const initialState = { count: 0 };
+
+  const reducer = (taskState, action) => {
+    switch (action.type) {
+      case "add":
+        return { count: taskState.count + 1 };
+      case "delete":
+        return { count: taskState.count - 1 };
+      case "deleteAll":
+        return { count: 0 };
+      default:
+        return taskState;
+    }
+  };
+
+  const [taskState, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div id="main">
       <h2>Task Counter</h2>
-      {/*<button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
-      <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
-      <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
-  <h3 className="counter">Number of Tasks : {taskState.count}</h3>*/}
+      <button onClick={() => dispatch({ type: "add" })} id="addTaskBtn">
+        Add a task
+      </button>
+      <button onClick={() => dispatch({ type: "delete" })} id="delTaskBtn">
+        Delete a task
+      </button>
+      <button
+        onClick={() => dispatch({ type: "deleteAll" })}
+        id="delAllTaskBtn"
+      >
+        Delete all tasks
+      </button>
+      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
 
-    {/*Update the above code to achieve the solution*/}
+      {/*Update the above code to achieve the solution*/}
     </div>
-  )
+  );
 }
